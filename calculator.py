6b99c8940c7641141ad51eea1fec4bce53199b92
james <james@JFS1-ADMIN-03>
2020-01-18 14:38:05 +0000

    comit ver 1

---
+++ calculator.py
@@ -0,0 +1,12 @@
+
+#ver 1
+print("number calculator")
+
+number_user = int(input("Number 1"))
+
+
+number_user2 = int(input("Number 2"))
+
+
+print("Number multipled is")
+print(number_user * number_user2)
